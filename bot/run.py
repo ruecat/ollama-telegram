e482d08fceed0912801a681b4cbde4e251b13c7e
Shriraj Hegde <shriraj.hegde@gmail.com>
2023-12-26 12:20:52 +0530

    Format python files with `black` Sort imports with `isort`

--- bot/run.py
+++ bot/run.py
@@ -1,34 +1,52 @@
-from func.controller import *
 import asyncio
+
 from aiogram import Bot, Dispatcher, types
-from aiogram.filters.command import CommandStart, Command
-from aiogram.utils.keyboard import InlineKeyboardBuilder
-from aiogram.types import Message
 from aiogram.enums import ParseMode
+from aiogram.filters.command import Command, CommandStart
+from aiogram.types import Message
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from func.controller import *
+
 bot = Bot(token=token)
 dp = Dispatcher()
 builder = InlineKeyboardBuilder()
-builder.row(types.InlineKeyboardButton(text="🤔️ Information", callback_data="info"),
-            types.InlineKeyboardButton(text="⚙️ Settings", callback_data="modelmanager"))
+builder.row(
+    types.InlineKeyboardButton(text="🤔️ Information", callback_data="info"),
+    types.InlineKeyboardButton(text="⚙️ Settings", callback_data="modelmanager"),
+)
 
-commands = [types.BotCommand(command="start", description="Start"),
-            types.BotCommand(command="reset", description="Reset Chat")]
+commands = [
+    types.BotCommand(command="start", description="Start"),
+    types.BotCommand(command="reset", description="Reset Chat"),
+]
 
 
 ACTIVE_CHATS = {}
 ACTIVE_CHATS_LOCK = contextLock()
 
-modelname = os.getenv('INITMODEL')
+modelname = os.getenv("INITMODEL")
+
+
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
     if message.from_user.id in allowed_ids:
         start_message = f"Welcome to OllamaTelegram Bot, ***{message.from_user.full_name}***!\nSource code: https://github.com/ruecat/ollama-telegram"
         start_message_md = md_autofixer(start_message)
-        await message.answer(start_message_md, parse_mode=ParseMode.MARKDOWN_V2, reply_markup=builder.as_markup(), disable_web_page_preview=True)
+        await message.answer(
+            start_message_md,
+            parse_mode=ParseMode.MARKDOWN_V2,
+            reply_markup=builder.as_markup(),
+            disable_web_page_preview=True,
+        )
     else:
         await message.answer(
-            f"{message.from_user.full_name} [AuthBlocked]\nContact staff to whitelist you", parse_mode=ParseMode.MARKDOWN_V2)
-        print(f"[Interactions] {message.from_user.username}({message.from_user.id}) is not allowed to use this bot. Value in environment: {allowed_ids}")
+            f"{message.from_user.full_name} [AuthBlocked]\nContact staff to whitelist you",
+            parse_mode=ParseMode.MARKDOWN_V2,
+        )
+        print(
+            f"[Interactions] {message.from_user.username}({message.from_user.id}) is not allowed to use this bot. Value in environment: {allowed_ids}"
+        )
+
 
 @dp.message(Command("reset"))
 async def command_reset_handler(message: Message) -> None:
@@ -37,36 +55,47 @@ async def command_reset_handler(message: Message) -> None:
             async with ACTIVE_CHATS_LOCK:
                 ACTIVE_CHATS.pop(message.from_user.id)
             print("DEBUG: Chat has been reset")
-            await bot.send_message(chat_id=message.chat.id, text="Chat has been reset",)
+            await bot.send_message(
+                chat_id=message.chat.id,
+                text="Chat has been reset",
+            )
 
 
-@dp.callback_query(lambda query: query.data == 'modelmanager')
+@dp.callback_query(lambda query: query.data == "modelmanager")
 async def modelmanager_callback_handler(query: types.CallbackQuery):
     if query.from_user.id in admin_ids:
         models = await model_list()
         modelmanager_builder = InlineKeyboardBuilder()
         for model in models:
-            modelname = model['name']
+            modelname = model["name"]
             # Add a button for each model
             modelmanager_builder.row(
-                types.InlineKeyboardButton(text=modelname, callback_data=f"model_{modelname}")
+                types.InlineKeyboardButton(
+                    text=modelname, callback_data=f"model_{modelname}"
+                )
             )
-            await query.message.edit_text("Choose model", reply_markup=modelmanager_builder.as_markup())
+            await query.message.edit_text(
+                "Choose model", reply_markup=modelmanager_builder.as_markup()
+            )
     else:
         await query.answer("Access Denied")
 
 
-@dp.callback_query(lambda query: query.data.startswith('model_'))
+@dp.callback_query(lambda query: query.data.startswith("model_"))
 async def model_callback_handler(query: types.CallbackQuery):
     global modelname
-    modelname = query.data.split('model_')[1]
+    modelname = query.data.split("model_")[1]
     await query.answer(f"Chosen model: {modelname}")
-@dp.callback_query(lambda query: query.data == 'info')
+
+
+@dp.callback_query(lambda query: query.data == "info")
 async def systeminfo_callback_handler(query: types.CallbackQuery):
     if query.from_user.id in admin_ids:
-        await bot.send_message(chat_id=query.message.chat.id,
-                               text=f"<b>📦 LLM</b>\n<code>Current model: {modelname}</code>\n\n🔧 Hardware\n<code>Kernel: {system_info[0]}\n</code>\n<i>(Other options will be added soon..)</i>",
-                               parse_mode="HTML")
+        await bot.send_message(
+            chat_id=query.message.chat.id,
+            text=f"<b>📦 LLM</b>\n<code>Current model: {modelname}</code>\n\n🔧 Hardware\n<code>Kernel: {system_info[0]}\n</code>\n<i>(Other options will be added soon..)</i>",
+            parse_mode="HTML",
+        )
     else:
         await query.answer("Access Denied")
 
@@ -79,9 +108,16 @@ async def handle_message(message: types.Message):
         is_private_chat = message.chat.type == "private"
         is_supergroup = message.chat.type == "supergroup"
         bot_mentioned = any(
-            entity.type == "mention" and message.text[entity.offset:entity.offset + entity.length] == f"@{botinfo.username}"
-            for entity in message.entities or [])
-        if is_allowed_user and message.text and (is_private_chat or (is_supergroup and bot_mentioned)):
+            entity.type == "mention"
+            and message.text[entity.offset : entity.offset + entity.length]
+            == f"@{botinfo.username}"
+            for entity in message.entities or []
+        )
+        if (
+            is_allowed_user
+            and message.text
+            and (is_private_chat or (is_supergroup and bot_mentioned))
+        ):
             if is_supergroup and bot_mentioned:
                 cutmention = len(botinfo.username) + 2
                 prompt = message.text[cutmention:]  # + ""
@@ -96,21 +132,13 @@ async def handle_message(message: types.Message):
             if ACTIVE_CHATS.get(message.from_user.id) is None:
                 ACTIVE_CHATS[message.from_user.id] = {
                     "model": modelname,
-                    "messages": [
-                        {
-                            "role":"user",
-                            "content": prompt
-                        }
-                    ],
-                    "stream": True
+                    "messages": [{"role": "user", "content": prompt}],
+                    "stream": True,
                 }
             else:
                 ACTIVE_CHATS[message.from_user.id]["messages"].append(
-                        {
-                            "role":"user",
-                            "content": prompt
-                        }
-                    )
+                    {"role": "user", "content": prompt}
+                )
             print(f"[Request]: Generating response for {prompt}")
             payload = ACTIVE_CHATS[message.from_user.id]
             async for response_data in generate(payload, modelname, prompt):
@@ -126,34 +154,40 @@ async def handle_message(message: types.Message):
                 if full_response_stripped == "":
                     continue
 
-                if '.' in chunk or '\n' in chunk or '!' in chunk or '?' in chunk:
+                if "." in chunk or "\n" in chunk or "!" in chunk or "?" in chunk:
                     if sent_message:
                         if last_sent_text != full_response_stripped:
                             await sent_message.edit_text(full_response_stripped)
                             last_sent_text = full_response_stripped
                     else:
-                        sent_message = await message.answer(
-                            full_response_stripped)
+                        sent_message = await message.answer(full_response_stripped)
                         last_sent_text = full_response_stripped
 
                 if response_data.get("done"):
-                    if full_response_stripped and last_sent_text != full_response_stripped:
+                    if (
+                        full_response_stripped
+                        and last_sent_text != full_response_stripped
+                    ):
                         if sent_message:
                             await sent_message.edit_text(full_response_stripped)
                         else:
                             sent_message = await message.answer(full_response_stripped)
-                    await sent_message.edit_text(md_autofixer(full_response_stripped + f"\n\nCurrent Model: `{modelname}`**\n**Generated in {response_data.get("total_duration")/10e9:.2f}s"), parse_mode=ParseMode.MARKDOWN_V2)
+                    await sent_message.edit_text(
+                        md_autofixer(
+                            full_response_stripped
+                            + f"\n\nCurrent Model: `{modelname}`**\n**Generated in {response_data.get('total_duration')/10e9:.2f}s"
+                        ),
+                        parse_mode=ParseMode.MARKDOWN_V2,
+                    )
 
                     # Add response to active chats object
 
                     ACTIVE_CHATS[message.from_user.id]["messages"].append(
-                        {
-                            "role":"assistant",
-                            "content": full_response_stripped
-                        }
+                        {"role": "assistant", "content": full_response_stripped}
                     )
 
                     break
+
 
 async def main():
     await bot.set_my_commands(commands)
